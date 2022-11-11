221886fbac424b182ce63077f261199f37c0b64b
jonasbergstrom <jontrian97@gmail.com>
2022-11-11 15:30:20 +0100

    Created the "TitleView" that will hold the titles from the fetched data

---
+++ Viaplay-assignment/View/TitleView.swift
@@ -0,0 +1,21 @@
+//
+//  TitleView.swift
+//  Viaplay-assignment
+//
+//  Created by Jonas Bergström on 2022-11-11.
+//
+
+import SwiftUI
+
+struct TitleView: View {
+    var title: Section
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10, content: {
+            
+            Text(title.title)
+                .font(.title2)
+                .fontWeight(.heavy)
+                .foregroundColor(.black)
+        })
+    }
+}

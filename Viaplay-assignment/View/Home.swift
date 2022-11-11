f32b101efd00a0916897579d93e60b46da4ffed7
jonasbergstrom <jontrian97@gmail.com>
2022-11-11 14:35:25 +0100

    Created  the "Home" view that will show the fetched data

---
+++ Viaplay-assignment/View/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  Viaplay-assignment
+//
+//  Created by Jonas Bergström on 2022-11-11.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        
+        VStack {
+            List(){
+                
+                // Will display the fetched JSON Data
+            }
+        }
+    }
+}

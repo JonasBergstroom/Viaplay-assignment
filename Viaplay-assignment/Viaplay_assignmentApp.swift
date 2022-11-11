019e1479b9aeed089e4e170a49d493cba21d2cc6
jonasbergstrom <jontrian97@gmail.com>
2022-11-11 14:24:13 +0100

    Initial Commit

---
+++ Viaplay-assignment/Viaplay_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Viaplay_assignmentApp.swift
+//  Viaplay-assignment
+//
+//  Created by Jonas Bergström on 2022-11-11.
+//
+
+import SwiftUI
+
+@main
+struct Viaplay_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
